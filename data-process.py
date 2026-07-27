6d282fe7d495d7b4e95fc33f3f6419c652d9cb3e
tidianesarrndiaye <tidianesarrndiaye04@gmail.com>
2026-07-27 15:03:32 +0100

    minor changes: adding file related to data-processing

---
+++ data-process.py
@@ -0,0 +1,32 @@
+def to_seconds(hours, minutes, seconds):
+    return hours*3600+minutes*60+seconds
+
+print("Welcome to this time converter")
+
+cont = "y"
+while(cont.lower() == "y"):
+    hours = int(input("Enter the number of hours: "))
+    minutes = int(input("Enter the number of minutes: "))
+    seconds = int(input("Enter the number of seconds: "))
+
+    print("That's {} seconds".format(to_seconds(hours, minutes, seconds)))
+    print()
+    cont = input("Do you want to do another conversion? [y to continue] ")
+    
+print("Goodbye!")
+
+
+"""
+echo $PATH
+/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+cat variables.py
+#!/usr/bin/env python3
+import os
+print("HOME: " + os.environ.get("HOME", ""))
+print("SHELL: " + os.environ.get("SHELL", ""))
+print("FRUIT: " + os.environ.get("FRUIT", ""))
+./variables.py
+export FRUIT=Pineapple
+./variables.py
+
+"""
