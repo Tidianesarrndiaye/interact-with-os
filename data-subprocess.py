4f7f4ac4b9bd61ded54a99afc3c0d8696af76141
tidianesarrndiaye <tidianesarrndiaye04@gmail.com>
2026-07-27 20:34:34 +0100

    minor changes

--- data-subprocess.py
+++ data-subprocess.py
@@ -1,6 +1,5 @@
 import subprocess
 
 print(subprocess.run(["date"]))
-
+print(subprocess.run(["sleep", "3"]))
 result = subprocess.run(["ls", "file_does_not_exist.txt"], capture_output=True, text=True)
-print(result)
